71011920edd6374e5035331f5761c2efd27dfbd2
Rodrigonobre <rodrigo.lima@pixeon.com>
2023-04-02 17:37:23 -0300

    implements methodo update

--- AnemicModelImproved.cs
+++ AnemicModelImproved.cs
@@ -6,10 +6,15 @@
         public string? Name { get; private set; }
         public string? Email { get; private set; }
 
-        public AnemicModelImproved(int id, string name, string email)
+        public AnemicModelImproved(string name, string email)
         {
             DataIsValid(name, email);
-            ID = id;
+            Name = name;
+            Email = email;
+        }
+        public void Update(string name, string email)
+        {
+            DataIsValid(name, email);
             Name = name;
             Email = email;
         }
@@ -17,10 +22,10 @@
         private void DataIsValid(string name, string email)
         {
             if (name is null || name == "")
-                throw new Exception("Name is invalid!");
+                throw new ArgumentException("Name is invalid!");
 
             if (!Util.EmailIsValid(email))
-                throw new Exception("Email is invalid!");
+                throw new ArgumentException("Email is invalid!");
         }
 
     }
