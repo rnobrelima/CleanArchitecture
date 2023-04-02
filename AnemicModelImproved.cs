3cf4802663ffb06b95f8a31fa0dd6a80b08bcb59
Rodrigonobre <rodrigo.lima@pixeon.com>
2023-04-02 17:30:01 -0300

    implements AnemicModelImproved

---
+++ AnemicModelImproved.cs
@@ -0,0 +1,27 @@
+﻿namespace CleanArchitecture
+{
+    sealed class AnemicModelImproved
+    {
+        public int ID { get; private set; }
+        public string? Name { get; private set; }
+        public string? Email { get; private set; }
+
+        public AnemicModelImproved(int id, string name, string email)
+        {
+            DataIsValid(name, email);
+            ID = id;
+            Name = name;
+            Email = email;
+        }
+
+        private void DataIsValid(string name, string email)
+        {
+            if (name is null || name == "")
+                throw new Exception("Name is invalid!");
+
+            if (!Util.EmailIsValid(email))
+                throw new Exception("Email is invalid!");
+        }
+
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchitecture
+{
+    static class Util
+    {
+        public static bool EmailIsValid(string email)
+        {
+            return new EmailAddressAttribute().IsValid(email);
+        }
+    }
+}
